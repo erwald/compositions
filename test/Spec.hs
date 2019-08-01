51411c345d8ba805082b83d0f246aab2779cf40c
Erich Grunewald <erich.grunewald@futurice.com>
2019-08-01 14:05:24 +0200

    Add tests for rotateL and rotateR

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,10 @@
 import Test.Hspec
 import Test.QuickCheck
 import Test.QuickCheck.Modifiers
-import Euclid
 import Control.Monad
+
+import Euclid
+import Utility
 
 main :: IO ()
 main = hspec $ do
@@ -12,16 +14,41 @@ main = hspec $ do
     describe "superEuclid" $ do
         it "should return # of distances equal to sum of ks" $ property prop_superEuclidLen
         it "distances should sum up to n" $ property prop_superEuclidSum
+    describe "rotateL" $ do
+        it "retain length of original lists" $ property prop_rotateLLen
+        it "retain sum of original lists" $ property prop_rotateLSum
+        it "wrap around when rotated a full lap" $ property prop_rotateLWraps
+    describe "rotateR" $ do
+        it "retain length of original lists" $ property prop_rotateRLen
+        it "retain sum of original lists" $ property prop_rotateRSum
+        it "wrap around when rotated a full lap" $ property prop_rotateRWraps
 
 prop_euclidLen (Positive k) (NonNegative n) = length (euclid k n) == k
 
 prop_euclidSum (Positive k) (NonNegative n) = sum (euclid k n) == n
 
-prop_superEuclidLen (NonNegative n) = forAll (nonNegativeInts n) $ \ks -> length (superEuclid ks n) == sum ks
+prop_superEuclidLen (NonNegative n) = forAll (nonEmptyNonNegativeInts n) $ \ks -> length (superEuclid ks n) == sum ks
 
-prop_superEuclidSum (NonNegative n) = forAll (nonNegativeInts n) $ \ks -> sum (superEuclid ks n) == n
+prop_superEuclidSum (NonNegative n) = forAll (nonEmptyNonNegativeInts n) $ \ks -> sum (superEuclid ks n) == n
 
-{-| @nonNegativeInts@ returns a generator of non-empty lists of integers with values between 1 and given value `n`.
+prop_rotateLLen (NonNegative n) = forAll (nonNegativeInts 100) $ \xs -> length (rotateL n xs) == length xs
+
+prop_rotateRLen (NonNegative n) = forAll (nonNegativeInts 100) $ \xs -> length (rotateR n xs) == length xs
+
+prop_rotateLSum (NonNegative n) = forAll (nonNegativeInts 100) $ \xs -> sum (rotateL n xs) == sum xs
+
+prop_rotateRSum (NonNegative n) = forAll (nonNegativeInts 100) $ \xs -> sum (rotateR n xs) == sum xs
+
+prop_rotateLWraps = forAll (nonNegativeInts 100) $ \xs -> rotateL (length xs) xs == xs
+
+prop_rotateRWraps = forAll (nonNegativeInts 100) $ \xs -> rotateR (length xs) xs == xs
+
+{-| @nonEmptyNonNegativeInts@ returns a generator of non-empty lists of integers with values between 1 and given value `n`.
+-}
+nonEmptyNonNegativeInts :: Int -> Gen [Int]
+nonEmptyNonNegativeInts n = oneof [return <$> choose (1, n), (:) <$> choose (1, n) <*> nonEmptyNonNegativeInts n]
+
+{-| @nonNegativeInts@ returns a generator of lists of integers with values between 1 and given value `n`.
 -}
 nonNegativeInts :: Int -> Gen [Int]
-nonNegativeInts n = oneof [return <$> choose (1, n), (:) <$> choose (1, n) <*> nonNegativeInts n]
+nonNegativeInts n = oneof [return [], (:) <$> choose (1, n) <*> nonNegativeInts n]
