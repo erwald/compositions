47d3aa633cd3c5b60f98e2e3ab81cced4ec335de
Erich Grunewald <erich.grunewald@futurice.com>
2019-08-02 16:44:48 +0200

    Add tests for euclidToMode function

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,7 @@ import Test.QuickCheck
 import Test.QuickCheck.Modifiers
 import Control.Monad
 import Data.Maybe (isNothing, isJust)
+import Data.List (sort)
 
 import Euclid
 import Utility
@@ -16,6 +17,10 @@ main = hspec $ do
         context "superEuclid" $ do
             it "should return # of distances equal to sum of ks" $ property prop_superEuclidLen
             it "distances should sum up to n" $ property prop_superEuclidSum
+        context "euclidToMode" $ do
+            it "should return list of correct length" $ property prop_euclidToModeLen
+            it "should return list with initial interval 0" $ property prop_euclidToModeInitial
+            it "should return list sorted ascendingly" $ property prop_euclidToModeOrdered
     describe "in Utility.hs" $ do
         context "nth" $ do
             it "returns Nothing for negative indices" $ property prop_nthNothingForNegativeIndices
@@ -36,9 +41,15 @@ prop_euclidLen (Positive k) (NonNegative n) = length (euclid k n) == k
 
 prop_euclidSum (Positive k) (NonNegative n) = sum (euclid k n) == n
 
-prop_superEuclidLen (NonNegative n) = forAll (nonEmptyNonNegativeInts n) $ \ks -> length (superEuclid ks n) == sum ks
+prop_superEuclidLen (NonNegative n) = forAll (nonEmptyPositiveInts n) $ \ks -> length (superEuclid ks n) == sum ks
 
-prop_superEuclidSum (NonNegative n) = forAll (nonEmptyNonNegativeInts n) $ \ks -> sum (superEuclid ks n) == n
+prop_superEuclidSum (NonNegative n) = forAll (nonEmptyPositiveInts n) $ \ks -> sum (superEuclid ks n) == n
+
+prop_euclidToModeLen = forAll (positiveInts 100) $ \ds -> length (euclidToMode ds) == length ds + 1
+
+prop_euclidToModeInitial = forAll (positiveInts 100) $ \ds -> head (euclidToMode ds) == 0
+
+prop_euclidToModeOrdered = forAll (positiveInts 100) $ \ds -> sort (euclidToMode ds) == euclidToMode ds
 
 prop_nthNothingForNegativeIndices (Negative i) xs = isNothing $ nth i xs where types = xs :: [Int]
 
@@ -49,26 +60,27 @@ prop_nthReturnsValueForIndicesInBounds (Positive n) xs = if null xs then isNothi
     f     = nth (n `mod` length xs) xs
     types = xs :: [Int]
 
-prop_rotateLLen (NonNegative n) = forAll (nonNegativeInts 100) $ \xs -> length (rotateL n xs) == length xs
+prop_rotateLLen (NonNegative n) = forAll (positiveInts 100) $ \xs -> length (rotateL n xs) == length xs
 
-prop_rotateRLen (NonNegative n) = forAll (nonNegativeInts 100) $ \xs -> length (rotateR n xs) == length xs
+prop_rotateRLen (NonNegative n) = forAll (positiveInts 100) $ \xs -> length (rotateR n xs) == length xs
 
-prop_rotateLSum (NonNegative n) = forAll (nonNegativeInts 100) $ \xs -> sum (rotateL n xs) == sum xs
+prop_rotateLSum (NonNegative n) = forAll (positiveInts 100) $ \xs -> sum (rotateL n xs) == sum xs
 
-prop_rotateRSum (NonNegative n) = forAll (nonNegativeInts 100) $ \xs -> sum (rotateR n xs) == sum xs
+prop_rotateRSum (NonNegative n) = forAll (positiveInts 100) $ \xs -> sum (rotateR n xs) == sum xs
 
-prop_rotateLWraps = forAll (nonNegativeInts 100) $ \xs -> rotateL (length xs) xs == xs
+prop_rotateLWraps = forAll (positiveInts 100) $ \xs -> rotateL (length xs) xs == xs
 
-prop_rotateRWraps = forAll (nonNegativeInts 100) $ \xs -> rotateR (length xs) xs == xs
+prop_rotateRWraps = forAll (positiveInts 100) $ \xs -> rotateR (length xs) xs == xs
 
 -- Generators
 
-{-| @nonEmptyNonNegativeInts@ returns a generator of non-empty lists of integers with values between 1 and given value `n`.
+{-| @nonEmptyPositiveInts@ returns a generator of non-empty lists of integers with values between 1 and given value `n`.
 -}
-nonEmptyNonNegativeInts :: Int -> Gen [Int]
-nonEmptyNonNegativeInts n = oneof [return <$> choose (1, n), (:) <$> choose (1, n) <*> nonEmptyNonNegativeInts n]
+nonEmptyPositiveInts :: Int -> Gen [Int]
+nonEmptyPositiveInts n = oneof [return <$> choosePositive, (:) <$> choosePositive <*> nonEmptyPositiveInts n]
+    where choosePositive = choose (1, n)
 
-{-| @nonNegativeInts@ returns a generator of lists of integers with values between 1 and given value `n`.
+{-| @positiveInts@ returns a generator of lists of integers with values between 1 and given value `n`.
 -}
-nonNegativeInts :: Int -> Gen [Int]
-nonNegativeInts n = oneof [return [], (:) <$> choose (1, n) <*> nonNegativeInts n]
+positiveInts :: Int -> Gen [Int]
+positiveInts n = oneof [return [], (:) <$> choose (1, n) <*> positiveInts n]
