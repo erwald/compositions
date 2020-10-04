a46d67e8427c7329f766f869c6d99d6c8da096b3
Erich Grunewald <erichgrunewald@gmail.com>
2020-10-04 03:04:30 +0200

    Add +spanUntilClosing function for grammar parsing

--- src/Utility.hs
+++ src/Utility.hs
@@ -4,6 +4,7 @@ module Utility
     , uncurry3
     , rotateL
     , rotateR
+    , spanUntilClosing
     )
 where
 
@@ -14,11 +15,11 @@ nth _ []       = Nothing
 nth 0 (x : _ ) = Just x
 nth n (_ : xs) = nth (n - 1) xs
 
-{- | Converts an uncurried function to a curried function. --}
+{-| Converts an uncurried function to a curried function. -}
 curry3 :: ((a, b, c) -> d) -> a -> b -> c -> d
 curry3 f a b c = f (a, b, c)
 
-{- | Converts a curried function to a function on a triple. --}
+{-| Converts a curried function to a function on a triple. -}
 uncurry3 :: (a -> b -> c -> d) -> ((a, b, c) -> d)
 uncurry3 f ~(a, b, c) = f a b c
 
@@ -28,3 +29,21 @@ rotateL n xs = zipWith const (drop (n `mod` length xs) (cycle xs)) xs
 
 rotateR :: Int -> [a] -> [a]
 rotateR n xs = rotateL (length xs - n) xs
+
+spanUntilClosing' :: Eq a => Int -> ([a], [a]) -> a -> a -> [a] -> ([a], [a])
+spanUntilClosing' _ (hd, tl) _ _ [] = (hd, tl)
+spanUntilClosing' depth (hd, tl) opener closer (x : xs)
+    | x == opener = spanUntilClosing' (depth + 1) (hd ++ [x], tl) opener closer xs
+    | x == closer = if (depth - 1) == 0
+        then (hd, tl ++ xs)  -- skip final closing bracket
+        else spanUntilClosing' (depth - 1) (hd ++ [x], tl) opener closer xs
+    | otherwise = spanUntilClosing' depth (hd ++ [x], tl) opener closer xs
+
+{-| Given a string that begins with an opening bracket, returns a tuple with, first, the contents 
+within those brackets, and second, the tail after the matching closing bracket. -}
+spanUntilClosing :: String -> (String, String)
+spanUntilClosing []         = ([], [])
+spanUntilClosing ('(' : xs) = spanUntilClosing' 1 ([], []) '(' ')' xs
+spanUntilClosing ('[' : xs) = spanUntilClosing' 1 ([], []) '[' ']' xs
+spanUntilClosing ('{' : xs) = spanUntilClosing' 1 ([], []) '{' '}' xs
+spanUntilClosing xs         = ([], xs)
